cdcc700c8a05a470f94a2a072b906b574ade7333
Jonkenn <jonkarlssonenbom@gmail.com>
2025-12-06 01:29:53 +0100

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -97,7 +97,7 @@ export default async function HomePage({
         <div className="max-w-7xl mx-auto w-full">
           <div className="flex flex-col gap-2">
             <h1 className="font-medium text-4xl md:text-5xl tracking-tighter">
-              Medborgarskapsprov.se
+              Medborgarskapsprov
             </h1>
             <p className="text-muted-foreground text-sm md:text-base lg:text-lg">
               Guider, nyheter och analyser om medborgarskapsprovet, migration och vardagsliv i Sverige.
